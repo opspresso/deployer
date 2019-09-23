ba50a5fb989ecc7152794bdc262e347de10478e6
nalbam <me@nalbam.com>
2019-09-23 18:27:18 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # deployer
+
+[![GitHub Actions status](https://github.com/opspresso/deployer/workflows/Build-Push/badge.svg)](https://github.com/opspresso/deployer/actions)
+[![GitHub Releases](https://img.shields.io/github/release/opspresso/deployer.svg)](https://github.com/opspresso/deployer/releases)
+
+[![DockerHub Badge](http://dockeri.co/image/opspresso/deployer)](https://hub.docker.com/r/opspresso/deployer/)
+
+```bash
+docker pull opspresso/deployer
+```
+
+## versions
+
+```
+ENV kubectl v1.15.4
+ENV awscli 1.16.210
+ENV helm v2.14.3
+```
---
+++ build.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+OS_NAME="$(uname | awk '{print tolower($0)}')"
+
+SHELL_DIR=$(dirname $0)
+
+REPOSITORY=${GITHUB_REPOSITORY}
+
+USERNAME=${GITHUB_ACTOR}
+REPONAME=$(echo "${REPOSITORY}" | cut -d'/' -f2)
+
+################################################################################
+
+# command -v tput > /dev/null && TPUT=true
+TPUT=
+
+_echo() {
+    if [ "${TPUT}" != "" ] && [ "$2" != "" ]; then
+        echo -e "$(tput setaf $2)$1$(tput sgr0)"
+    else
+        echo -e "$1"
+    fi
+}
+
+_result() {
+    echo
+    _echo "# $@" 4
+}
+
+_command() {
+    echo
+    _echo "$ $@" 3
+}
+
+_success() {
+    echo
+    _echo "+ $@" 2
+    exit 0
+}
+
+_error() {
+    echo
+    _echo "- $@" 1
+    exit 1
+}
+
+_replace() {
+    if [ "${OS_NAME}" == "darwin" ]; then
+        sed -i "" -e "$1" $2
+    else
+        sed -i -e "$1" $2
+    fi
+}
+
+_prepare() {
+    # target
+    mkdir -p ${SHELL_DIR}/target/publish
+    mkdir -p ${SHELL_DIR}/target/release
+
+    # 755
+    find ${SHELL_DIR}/** | grep [.]sh | xargs chmod 755
+}
+
+################################################################################
+
+_package() {
+    _check_version "helm"
+    _check_version "kubectl"
+
+    if [ ! -z "${CHANGED}" ]; then
+        _check_version "awscli"
+    fi
+}
+
+_check_version() {
+    NAME=${1}
+
+    NOW=$(cat ${SHELL_DIR}/Dockerfile | grep "ENV ${NAME}" | awk '{print $3}' | xargs)
+
+    NEW=$(curl -sL repo.opspresso.com/latest/${NAME} | xargs)
+
+    if [ -z "${NEW}" ]; then
+        return
+    fi
+
+    _result "$(printf '%-30s %-25s %-25s' "${NAME}" "${NOW}" "${NEW}")"
+
+    if [ "${NEW}" != "${NOW}" ]; then
+        CHANGED=true
+
+        # replace
+        _replace "s/ENV ${NAME} .*/ENV ${NAME} ${NEW}/g" ${SHELL_DIR}/Dockerfile
+        _replace "s/ENV ${NAME} .*/ENV ${NAME} ${NEW}/g" ${SHELL_DIR}/README.md
+    fi
+}
+
+################################################################################
+
+_prepare
+
+_package
